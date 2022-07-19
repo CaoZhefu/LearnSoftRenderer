481d83665e1c9bc3003e883addf3d04da1d97e1e
CaoZhefu <897772958@qq.com>
2022-07-19 17:33:17 +0800

    write bmp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
-#include "myMath.h"
 #include <iostream>
+#include "myMath.h"
+#include "renderer.h"
 
 int main()
 {
-    std::cout << "test " << std::endl;
-    std::cin.get();
+    renderer r(300, 200);
+    r.fill(vec4(255, 255, 255, 1.f));
+    r.saveToFile("out.bmp");
+
+#if defined(WIN32) || defined(_WIN32)
+	system("mspaint out.bmp");
+#endif
+
     return 0;
 }
--- renderer.h
+++ renderer.h
@@ -1,38 +1,51 @@
+#pragma once
+
 #include "myMath.h"
+
+#ifndef STB_IMAGE_WRITE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#endif
+
+#include "stb_image_write.h"
+#include <string>
+#include <fstream>
 
 class renderer
 {
 public:
     int width, height;
-    vec4** frameBuffer;
-    float** zBuffer;
+    vec4* frameBuffer;
+    float* zBuffer;
 
     renderer(int w, int h) : width(w), height(h){
-        frameBuffer = new vec4*[w];
-        zBuffer = new float*[w];
-
-        for(int i = 0; i < w; ++i){
-            frameBuffer[i] = new vec4[h];
-            zBuffer[i] = new float[h];
-        }
+        frameBuffer = new vec4[w * h];
+        zBuffer = new float[w * h];
     }
 
-    void drawPixel(int x, int y, vec4& color){
+    void drawPixel(int x, int y, const vec4& color){
         if(x >= 0 && x < width && y >= 0 && y < height)
-            frameBuffer[x][y] = color;
+            frameBuffer[width * y + x] = color;
     }
 
-    // 保存为bmp文件
-    void saveToFile(const char* path){
+    void fill(const vec4& color){
+        for(int i = 0; i < width * height; ++i)
+            frameBuffer[i] = color;
+    }
 
+    // 保存为文件
+    void saveToFile(const std::string& path){
+        unsigned char* data = new unsigned char[width * height * 3];
+        for(int i = 0; i < width * height; ++i){
+            data[i * 3 + 0] = frameBuffer[i].x;
+            data[i * 3 + 1] = frameBuffer[i].y;
+            data[i * 3 + 2] = frameBuffer[i].z;
+        }
+
+        stbi_write_bmp(path.c_str(), width, height, 3, data);
+        delete[] data;
     }
 
     ~renderer(){
-        for(int i = 0; i < width; ++i){
-            delete[] frameBuffer[i];
-            delete[] zBuffer[i];
-        }
-
         delete[] frameBuffer;
         delete[] zBuffer;
     }
